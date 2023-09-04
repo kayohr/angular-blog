bf677d5d9dea68b48cfe05c344fc51fba40655a6
kayo <kayohramos@gmail.com>
2023-09-04 20:46:43 -0300

    style, multiplos style component

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css', 'big-card.component.respon.css']
 })
 export class BigCardComponent {
 
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css', 'small-card.component.respon.css']
 })
 export class SmallCardComponent {
 
